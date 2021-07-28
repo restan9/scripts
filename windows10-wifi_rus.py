adf2cc451d259fd14e070e278b6e834a6e3ca6b9
restan94 <anton.nesterovich.94@mail.ru>
2021-07-28 15:13:14 +0300

    Update windows10-wifi_rus.py

--- windows10-wifi_rus.py
+++ windows10-wifi_rus.py
@@ -1,8 +1,3 @@
-######################################
-#Copyright of David Bombal, 2021     #
-#https://www.davidbombal.com         #
-#https://www.youtube.com/davidbombal #
-######################################
 
 #    Import subprocess so we can use system commands.
 import subprocess
